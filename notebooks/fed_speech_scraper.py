e9a8bad2b1292ed09895597883bd4209c6a496bd
wbzhang233 <wbzhang233@163.com>
2024-12-05 21:35:53 +0800

    定时调度程序开发

--- notebooks/fed_speech_scraper.py
+++ notebooks/fed_speech_scraper.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
+"""
 @File    :   fed_speech_scraper.py
 @Time    :   2024/12/04 10:28:05
-@Author  :   wbzhang 
+@Author  :   wbzhang
 @Version :   1.0
 @Desc    :   FED演讲稿数据爬取运行
-'''
+"""
 
+import schedule
+import time
 from data_scraper.scrapers import (
     BOGSpeechScraper,
     BostonSpeechScraper,
@@ -42,11 +44,33 @@ SCRAPERS = {
     # "fraserfrb": FRASERFRBSpeechScraper
 }
 
-if __name__ == "__main__":
+
+def main():
     # 编排运行所有爬虫程序. 串行执行
     print(
-        "="*50 + " Federal Reservel Speeches Scraper Starting... " + "="*50 + "\n"
+        "=" * 50 + " Federal Reservel Speeches Scraper Starting... " + "=" * 50 + "\n"
     )
     for name, _scraper in SCRAPERS.items():
         scraper = _scraper()
         scraper.collect()
+    return False
+
+
+def job_scheduler():
+    should_continue = True
+
+    def scheduled_main():
+        nonlocal should_continue
+        result = main()
+        if result is False:
+            should_continue = False
+
+    schedule.every().day.at("02:00").do(scheduled_main)
+    while should_continue:
+        print("Checking for scheduled tasks...")
+        schedule.run_pending()
+        time.sleep(60)  # 每分钟检查一次是否有任务需要执行
+
+
+if __name__ == "__main__":
+    job_scheduler()
